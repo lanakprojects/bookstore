e3ccada2bd1489bdf2801c58e49c876af9d7c2c1
unknown <lanawwk@gmail.com>
2020-10-03 17:23:29 +0300

    Spring Security Part 2

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -11,6 +11,8 @@ import com.example.bookstore.domain.Book;
 import com.example.bookstore.domain.BookRepository;
 import com.example.bookstore.domain.Category;
 import com.example.bookstore.domain.CategoryRepository;
+import com.example.bookstore.domain.User;
+import com.example.bookstore.domain.UserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -21,17 +23,27 @@ public class BookstoreApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner studentDemo(BookRepository repository, CategoryRepository catrepository) {
+	public CommandLineRunner studentDemo(BookRepository repository, CategoryRepository catrepository, UserRepository userrepository) {
 		return (args) -> {
 			log.info("insert example data");
 			
+			// Add categories
 			catrepository.save(new Category("novel"));
 			catrepository.save(new Category("fantasy"));
 			catrepository.save(new Category("detective"));
 			
+			// Add books
 			repository.save(new Book("Pride and Prejudice", "Jane Austen", 1813, "book1813", 18.13, catrepository.findByName("novel").get(0)));
 			repository.save(new Book("One Hundred Years of Solitude", "Gabriel Garcia Marquez", 1967, "book1967", 1967, catrepository.findByName("fantasy").get(0)));
 			repository.save(new Book("The Flowers of Evil", "Charles Baudelaire", 1857, "book1857", 1857, catrepository.findByName("novel").get(0)));
+			
+			/* Add users hashing passwords with BCrypt 
+			 * user1: user / userPassword
+			 * user2: admin / adminPassword */
+			User user1 = new User("user", "$2a$10$76w5DpGVrvumJet.iOFScePRV6CBR69JAGXU3VL4tnArscoyee3vO", "user@email.fi", "USER");
+			User user2 = new User("admin", "$2a$10$fOtNe7L89knEv9.FcMciveennu3f.RNIkONu0oam7HF5EuywmF3He", "admin@email.fi", "ADMIN");
+			userrepository.save(user1);
+			userrepository.save(user2);
 			
 			log.info("fetch all books");
 			for (Book book : repository.findAll()) {
--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -59,6 +60,7 @@ public class BookController {
 	}
 
 	// Delete a book
+	@PreAuthorize("hasAuthority('ADMIN')")
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 		repository.deleteById(bookId);
